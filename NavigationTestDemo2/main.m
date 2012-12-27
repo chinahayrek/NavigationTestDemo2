4c03422f723d06fa7131701139c397e106dac379
liumingbo <liumingbo@liumingbotekiMacBook-Pro.local>
2012-12-27 15:46:18 +0800

    Initial Commit

---
+++ NavigationTestDemo2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NavigationTestDemo2
+//
+//  Created by liumingbo on 12-12-27.
+//  Copyright (c) 2012年 com.chinahayrek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
